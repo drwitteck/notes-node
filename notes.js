614e974849483a614084a2f21be5455b39ccdc24
Derek Witteck <drwitteck@gmail.com>
2018-02-05 22:30:52 -0500

    Initial commit

---
+++ notes.js
@@ -0,0 +1,10 @@
+console.log('Starting notes.js');
+
+module.exports.addNotes = () => {
+    console.log('addNote');
+    return 'New note';
+};
+
+module.exports.add = (a, b) => {
+    return a + b;
+};
